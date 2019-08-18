4f63b4617503ff1de4b7b98ce5c3d532cd497e23
unknown <ajohns@yandex.ru>
2019-08-18 13:39:05 +0300

    First deploy

---
+++ BotEnterp.py
@@ -0,0 +1,30 @@
+# -*- coding: cp1251 -*-
+import telebot
+from telebot import apihelper
+import apiai, json
+from apiai.apiai import ApiAI
+
+
+apihelper.proxy = {'https':'socks5://mtpro_xyz:mtpro_xyz_bot@exp1.s5overss.mtpro.xyz:39610'}
+
+bot = telebot.TeleBot(API_TOKEN)
+bot.remove_webhook()
+
+'''@bot.message_handler(commands=['start'])
+def startCommand(message):
+    bot.send_message(chat_id=message.chat.id, text='Lets get started!')'''
+@bot.message_handler(func=lambda message: True)
+def textMessage(message):
+    request = apiai.ApiAI(AItok).text_request()
+    request.lang = 'eng'
+    request.session_id = 'BotTalk'
+    request.query = message.text
+    responseJson = json.loads(request.getresponse().read().decode('utf-8'))
+    response = responseJson['result']['fulfillment']['speech']
+    if response:
+        bot.send_message(chat_id=message.chat.id, text=response)
+    else:
+        bot.send_message(chat_id=message.chat.id, text='I dont understand you')
+#bot.send_message(chat_id=message.chat.id, text=message.text)
+
+bot.polling(none_stop=True, timeout=123)
